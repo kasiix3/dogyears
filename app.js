b01416730dd53913f3c5ad3b6e3f0c14a7c7f415
Kasandra  Kleespie <87586209+kasiix3@users.noreply.github.com>
2021-11-17 13:04:17 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+//Estabilishing variables
+var myAge = 26;
+var earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+//age in dog years
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+//my name
+var myName = 'Kas';
+console.log(myName.toLowerCase())
+console.log( `My name is ${myName}. I am ${myAge} years old in human years which is ${laterYears} in dog years.`)
